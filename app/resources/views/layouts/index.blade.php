78d36fcb5c3e114d26d4fd29348b1651680cc812
Mai Van Quang <maiquang1470@gmail.com>
2021-08-11 21:04:45 +0700

    cat giao dien

---
+++ app/resources/views/layouts/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sinh viên - VKU</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="/css/cdn/bootstrap.min.css" type="text/css" rel="stylesheet" />
+    <link href="/js/cdn/bootstrap.min.js" type="text/javascript" />
+    <link href="/js/cdn/popper.min.js" type="text/javascript"rel="stylesheet" />
+    <link href="/css/home.css" type="text/css" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
+    @yield('css')
+    <meta name="csrf-token" value="{{ csrf_token() }}" />
+</head>
+
+<body>
+    <section class='w-100' style='height: 100vh'>
+        @yield('header')
+        <section class='d-flex overflow-x-hidden' style='height: 100vh'>
+            <!-- SIDEBAR SECTION -->
+            @yield('sidebar')
+                <!-- SIDEBAR SECTION -->
+            <div style='padding: 70px 0px 0px; width: 100%; height: 100%;'>
+                @yield('content')
+            </div>
+        </section>
+    </section>
+    @yield('js')
+</body>
+
+</html>
---
+++ app/resources/views/layouts/sv/header.blade.php
@@ -0,0 +1,40 @@
+<div>
+    <nav class="navbar navbar-light bg-white" id='nav'>
+        <div class="container-fluid d-flex align-items-center justify-content-between">
+            <div>
+                <i class='fa fa-bars' style='font-size: 26px; cursor: pointer' onClick='openSider()'> </i>
+            </div>
+            <div class='nav-right position-relative'>
+                <div>
+                    <img class='rounded-circle' src='http://daotao.vku.udn.vn/uploads/sinhvien/18IT285.jpg' />
+                    <span>
+                        @if (Auth::user())
+                            {{ Auth::user()->name }}
+                        @endif Nga - 18IT5
+                    </span>
+                </div>
+                <div class='position-absolute user-dropdown'>
+                    <div>
+                        <div class='dropdown-arrow'></div>
+                        <div class='user-dropdown-content bg-white ' 
+                        style='padding: 12px 16px; box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);'>
+                            {{-- <a href='/' class='dropdown-tag'>
+                                <span><i class='fa fa-sign-out'></i></span>
+                                <span>Logout</span>
+                            </a> --}}
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                <span><i class='fa fa-sign-out'></i></span>
+                                <span>Logout</span>
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
