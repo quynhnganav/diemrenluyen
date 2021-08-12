88549566c01e92db73c3620fa45e55459d25dcdc
quynhnganav <quynhnganav.com>
2021-08-12 18:05:10 +0700

    update layout

--- app/resources/views/layouts/sv/sidebar.blade.php
+++ app/resources/views/layouts/sv/sidebar.blade.php
@@ -7,15 +7,8 @@
     <div class='position-relative w-100 menu-wrap sidebar-left' style='height: calc(100% - 70px); max-height: calc(100% - 70px);'>
         <div class='position-absolute top-0'
             style='position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin-right: 0px; margin-bottom: 0px;'>
-            <ul class='py-4'>
-                <li class='d-block'>
-                    <a class='text-decoration-none  text-white '>
-                        <span style='color: rgb(120, 129, 149)'>
-                            <i class='fa fa-street-view'></i>
-                            <span class='ps-2' id='menu-text'>Lí lịch sinh viên</span>
-                        </span>
-                    </a>
-                </li>
+            <ul class='py-4 ' id='siderbar'>
+            
             </ul>
         </div>
     </div>
@@ -24,10 +17,12 @@
 @section('js')
     @parent
     <script>
+        var test = false
         function openSider() {
+            test = !test
             document.getElementById("sider")?.classList.toggle('openSider');
             document.getElementById("sider")?.classList.toggle('closeSider');
-            document.getElementById("menu-text")?.classList.toggle('d-none');
+            document.getElementById("sider")?.querySelector('a > span').classList.toggle('d-none');
             document.getElementById("nav")?.classList.toggle('togge-padding-left');
         }
         function checkWidthToggle() {
@@ -46,5 +41,37 @@
         if (w < 768) {
             checkWidthToggle()
         }
+        var siderItems = [
+   {
+       key: '/',
+       title: 'Lí lịch sinh viên',
+       icon : 'fa-street-view'  
+   },
+   {
+        key: 'calendar',
+        title: 'Thời khoá biểu',
+        icon : 'fa-calendar' ,
+        childrens: [{
+        key: 'calendar-week',
+        title: 'Thời khoá biểu tuần',
+        icon : 'fa-calendar' , 
+        }]  
+   }
+]
+document.getElementById('siderbar').innerHTML = siderItems.map(item => 
+    `<li class='d-block py-2' key=${item.key}>
+    <a class='text-decoration-none text-white' href='${item.key}'>
+    <span style='color: rgb(120, 129, 149)'>
+    <i class='fa ${item.icon}'></i>
+    <span class='ps-2 isDisplay'>${item.title}</span>
+    </span>
+    </a>
+    </li>
+    ${item.children == undefined}  ? <p>a</p> : <p>b</p>}
+    `
+).join('')
     </script>
+    <script>
+  
+</script>
 @endsection
